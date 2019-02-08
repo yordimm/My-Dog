e73bd7dc50b3d267431d6925af3357076a3b6331
unknown <yordimosq@hotmail.com>
2019-02-08 11:57:57 -0500

    Se realizan pruebas unitarias del componente DogCard

--- src/Test/DogCard/index.test.js
+++ src/Test/DogCard/index.test.js
@@ -7,11 +7,12 @@ const dogInfo = {
     image: 'https://images.dog.ceo/breeds/bluetick/n02088632_3888.jpg'
 }
 const { breed, image } = dogInfo;
+const component = renderer.create(
+    <DogCard image={image} name={breed} />,
+);
+const instance = component.root;
 
 test('Shows a Card with an image and a name', () => {
-    const component = renderer.create(
-        <DogCard image={image} name={breed} />,
-    );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 });
@@ -19,25 +20,21 @@ test('Shows a Card with an image and a name', () => {
 
 describe('Rendered elements', () => {
     test('should show the breed of the dog', () => {
-        const component = renderer.create(
-            <DogCard image={image} name={breed} />,
-        );
-        expect(component.find('.breed').text().toEqual(breed))
+        const name = instance.find((el) => el.type == 'p')
+        expect(name.props.children).toEqual(breed)
     });
 
     test('should not show anything if there is not a name prop', () => {
-        const component = renderer.create(
-            <DogCard image={image}/>,
+        const dogCard = renderer.create(
+            <DogCard image={image} />,
         );
-        console.log(component.toJSON())
-        expect(component.find('.breed').text().toEqual(''))
+        const dogInstance = dogCard.root;
+        const name = dogInstance.find((el) => el.type == 'p')
+        expect(name.props.children).toBe('')
     });
 
     test('should show the dog´s image', () => {
-        const component = renderer.create(
-            <DogCard image={image} name={breed} />,
-        );
-        const img= component.find('img');
-        expect(img.prop('src').toEqual(image))
+        const dogImage = instance.find((el) => el.type == 'img')
+        expect(dogImage.props.src).toEqual(image)
     });
 });
